c3040c5b2903110041055005a5336ac7867a1e59
Marciano Salazar <168376936+Sheyxii@users.noreply.github.com>
2026-07-21 21:58:44 +0800

    feat: Add SQL script for dimension exploration queries
    This SQL script retrieves unique values for customer types, product categories, territory groups, and sales persons.

---
+++ scipts/data_analysis/dimension_exploratio.sql
@@ -0,0 +1,29 @@
+
+-- Retrieve a list of unique customer type
+SELECT DISTINCT 
+    customer_type 
+FROM gold.dim_customers
+ORDER BY customer_type;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
+
+-- Retrieve a list of unique group name, country/region code, and name
+SELECT DISTINCT 
+    group_name, 
+    country_region_code, 
+    name 
+FROM gold.dim_territory 
+ORDER BY group_name, country_region_code, name;
+
+-- Retrieve a list of unique job title, sales person name
+SELECT DISTINCT 
+    job_title,
+    full_name 
+FROM gold.dim_sales_persons 
+ORDER BY job_title, full_name ;
