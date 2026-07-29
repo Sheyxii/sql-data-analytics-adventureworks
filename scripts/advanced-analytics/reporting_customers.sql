d573cad36add14d3b3d44a8cabe0147912b5bf8f
Marciano Salazar <168376936+Sheyxii@users.noreply.github.com>
2026-07-29 19:41:06 +0800

    feat: Modify reporting_customers.sql to manage view creation
    Drop existing view if it exists before creating a new one.

--- scripts/advanced-analytics/reporting_customers.sql
+++ scripts/advanced-analytics/reporting_customers.sql
@@ -32,7 +32,11 @@ Highlights:
 -- =============================================================================
 -- Create Report: gold.report_customers
 -- =============================================================================
+IF OBJECT_ID('gold.report_customers', 'V') IS NOT NULL
+    DROP VIEW gold.report_customers;
+GO
 
+CREATE VIEW gold.report_customers AS 
 
 /*---------------------------------------------------------------------------
 1) Base Query: Retrieve core columns from fact_sales and dim_customers
